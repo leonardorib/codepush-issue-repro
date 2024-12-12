41e7f16ff88778e0027d7ef033ea5c6b56255ed5
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-12-12 13:17:26 -0300

    Installed and configured codepush for iOS

--- ios/ReproCodepushBuildIssue/AppDelegate.mm
+++ ios/ReproCodepushBuildIssue/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <CodePush/CodePush.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -24,7 +26,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
